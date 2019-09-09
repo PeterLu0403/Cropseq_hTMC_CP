0f32f2f215181efdf1a9eb053b6da5df56a4d1e4
Peter Lu <qinyil@DOM-WDC-8ZJC7H2.health.utas.edu.au>
2019-09-09 11:45:57 +1000

    Updating test

--- loading_data.R
+++ loading_data.R
@@ -1,1 +1,10 @@
 ## Read sql files:
+library("RSQLite")
+
+sql.path<-"../../Database/hTMC_analysis.db"
+db <- dbConnect(RSQLite::SQLite(), sql.path)
+tables<-dbListTables(db)
+lDataFrames <- vector("list", length=length(tables))
+for (i in seq(along=tables)) {
+  lDataFrames[[i]] <- dbGetQuery(conn=db, statement=paste("SELECT * FROM '", tables[[i]], "'", sep=""))
+}
